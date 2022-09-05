ffa51a2fe9e30f7eedcb81bfb0b1482b496872bd
JungHan Seo <junghanseo84@gmail.com>
2022-09-05 13:50:42 -0700

    create README file and write server.js and seeds.sql

--- server.js
+++ server.js
@@ -1,23 +1,65 @@
-const express = require('express');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table")
 
-//Import and require mysql2
-const mysql = require('mysql2');
+const connection = mysql.createConnection({
+   host:"localhost",
+   port: 3000,
+   user: "root",
+   password: "password",
+   database: "employee_db"
+});
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+connection.connect(function(err){
+  if(err) throw err;
 
-//Express middleware
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+  console.log("Conneted as ID" + connection.threadId);
+  console.clear();
+  console.log("=================");
+  console.log("");
+  console.log("  Welcom to the Employee Database  ");
+  console.log("");
+  console.log("=================");
+  runEmployeeDB();
+});
 
-//Connetct to database
-const db = mysql.createConnection(
-   {
-      host:'localhost',
-      //MySQL username,
-      user:'root',
-      //TODE:Add MySQL password here
-      password:'',
-      database:'movie_db'
-   }
-)
+//List of User Type
+
+function runEmployeeDB(){
+   inquirer.createPromptModule([
+      {
+         type: "list",
+         message: "What would like to do today?",
+         name: "action",
+         choices: [
+            "view All Employees",
+            "view All Departments",
+            "view All Roles",
+            "view All Employees by Department",
+            "view All Employees by Role",
+            "Add Department",
+            "Add Role",
+            "Add Employees",
+            "Update Employee Role",
+            "Exit"
+         ]
+      }
+   ]).then(function(answers){
+       switch (answers.action){
+
+         //View All Employee
+         case "View All Employees":
+            viewAllEmployees();
+         break;
+         
+         //View All Roles
+         case "View All Roles":
+            viewAllRoles();
+         break;
+
+         //View All Employees by dep
+         case "View All Employees by Role":
+            viewAllEmployeesByRole();
+         break;
+   })
+}
